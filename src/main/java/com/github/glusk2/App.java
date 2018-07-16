525192e9b7677835a0809691d94be625cb94d075
Glusk2 <lj.slo@hotmail.com>
2018-07-16 19:33:30 +0200

    Added: Eclipse gitignore; basic JavaFX App; mvn config

--- src/main/java/com/github/glusk2/App.java
+++ src/main/java/com/github/glusk2/App.java
@@ -1,13 +1,17 @@
 package com.github.glusk2;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Tečajnica");
+        primaryStage.show();
     }
 }
