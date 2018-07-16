220c68d26df6adb4e22904b3f312c1edab12c0a1
Glusk2 <lj.slo@hotmail.com>
2018-07-16 18:26:22 +0200

    Added maven structure.

---
+++ src/main/java/com/github/glusk2/App.java
@@ -0,0 +1,13 @@
+package com.github.glusk2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
